7c3227725c22ef71f9dc84b47b44b656d667e2ae
Ankit <ankiisnap@gmail.com>
2020-08-03 12:19:56 +0530

    visually impaired feature added

--- User-app/Constants/faqs.Infodart.dart
+++
@@ -1,8 +0,0 @@
-var heading = [
-  "How can I pay for my ride?",
-  "How can I select my starting point and destination?",
-  "How can I track my bus?",
-  "When can I use Emergency button?",
-  "How can I use Share Location?",
-];
-var message = [];
